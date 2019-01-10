7a2753cc6465673b07812ecb1bf2db0fb62f3db2
Benson Lee <benson.lee@amethodschools.org>
2019-01-10 01:58:36 +0000

    list out the songs links

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = [
+    "Congratulations",
+    "Better Now",
+    "White Iverson"
+    "it's different"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var links = [
+    "https://www.youtube.com/watch?v=SC4xMk98Pdc",
+    "https://www.youtube.com/watch?v=UYwF-jdcVjY",
+    "https://www.youtube.com/watch?v=SLsTskih7_I",
+    "https://www.youtube.com/watch?v=UHbDJ1oPuGo"
+    ]
 
 
 
@@ -15,6 +25,7 @@ var songs = [];
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
+    var emptySongInfo=
 
 
 }
